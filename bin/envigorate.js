a02d4be80ae4a0ca2d83ba92b98c361fbaaa00f7
Stuart P. Bentley <stuart@testtrack4.com>
2014-12-11 13:03:18 -0800

    envigorate: Fix not-double-require

--- bin/envigorate.js
+++ bin/envigorate.js
@@ -3,5 +3,5 @@
 var cfg = require('envigor')();
 
 require('http')
-  .createServer(require('path').resolve(process.argv[2] || '.')(cfg))
+  .createServer(require(require('path').resolve(process.argv[2] || '.'))(cfg))
   .listen(cfg.port || 3000, cfg.ip);
